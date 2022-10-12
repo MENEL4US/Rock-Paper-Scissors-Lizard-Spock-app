ea2967cb453edd8ef03b870c99ae122769c4a4b4
MENEL4US <luizrodriguesbmm@gmail.com>
2022-10-12 20:11:06 -0300

    add messages

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: implementation_imports, prefer_const_constructors, unnecessary_import, avoid_unnecessary_containers, avoid_print, prefer_final_fields, unnecessary_this
+// ignore_for_file: implementation_imports, prefer_const_constructors, unnecessary_import, avoid_unnecessary_containers, avoid_print, prefer_final_fields, unnecessary_this, prefer_interpolation_to_compose_strings
 
 import 'dart:math';
 
@@ -14,55 +14,105 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List options = ["rock", "paper", "scissors", "lizard", "spock"];
+  List options = ["rock", "paper", "scissor", "lizard", "spock"];
 
   var wins = {
-    "rock": ["lizard", "scissors"],
+    "rock": ["lizard", "scissor"],
     "paper": ["rock", "spock"],
-    "scissors": ["lizard", "paper"],
+    "scissor": ["lizard", "paper"],
     "lizard": ["spock", "paper"],
-    "spock": ["rock", "scissors"],
+    "spock": ["rock", "scissor"],
+  };
+
+  var results = {
+    "rock": {
+      "lizard": "Pedra esmaga Lagarto",
+      "scissor": "Pedra amassa Tesoura",
+    },
+    "paper": {
+      "rock": "Papel cobre Pedra",
+      "spock": "Papel refuta Spock",
+    },
+    "scissor": {
+      "paper": "Tesoura corta Papel",
+      "lizard": "Tesoura decapita Lagarto",
+    },
+    "lizard": {
+      "spock": "Lagarto envenena Spock",
+      "paper": "Lagarto come Papel",
+    },
+    "spock": {
+      "rock": "Spock vaporiza Pedra",
+      "scissor": "Spock derrete Tesoura",
+    },
   };
 
   String _message = "Escolha uma opção";
   String _imagePath = "images/default.png";
+  String _result = "";
+  int _vitorias = 0;
+  int _derrotas = 0;
+  int _empates = 0;
+
+  void _reset() {
+    setState(() {
+      this._message = "Escolha uma opção";
+      this._imagePath = "images/default.png";
+      this._vitorias = 0;
+      this._empates = 0;
+      this._derrotas = 0;
+      this._result = "";
+    });
+  }
 
   void _play(String choice) {
     int i = Random().nextInt(options.length);
     String computer = options[i];
     String newMessage = "";
+    String newResult = "";
+
+    int newVitorias = _vitorias;
+    int newDerrotas = _derrotas;
+    int newEmpates = _empates;
 
     if (choice == computer) {
-      newMessage = "Empate!";
+      newEmpates++;
+      newResult = "";
+      newMessage = "Empatamos!";
     } else if (wins[choice]!.contains(computer)) {
-      newMessage = "Vitória! :D";
+      newVitorias++;
+      newMessage = "Você venceu!";
+      newResult = results[choice]![computer]!;
     } else {
-      newMessage = "Derrota! :(";
+      newMessage = "Você perdeu!";
+      newResult = results[computer]![choice]!;
+      newDerrotas++;
     }
 
     setState(() {
       this._message = newMessage;
       this._imagePath = "images/$computer.png";
+      this._vitorias = newVitorias;
+      this._empates = newEmpates;
+      this._derrotas = newDerrotas;
+      this._result = newResult;
     });
-
-    print("You: $choice, Computer: $computer $newMessage");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        titleTextStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         title: Text("Pedra, Papel, Tesoura, Lagarto ou Spock"),
       ),
       body: Container(
-        // main
         child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Padding(
               padding: EdgeInsets.only(
-                top: 24,
-                bottom: 24,
+                top: 12,
+                bottom: 12,
               ),
               child: Text(
                 "Escolha do App",
@@ -72,19 +122,42 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            Image.asset(
-              _imagePath,
-              height: 160,
+            // Image.asset(
+            //   _imagePath,
+            //   height: 90,
+            // ),
+            GestureDetector(
+              onTap: () => _reset(),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Image.asset(
+                  _imagePath,
+                  height: 90,
+                ),
+              ),
             ),
             Padding(
               padding: EdgeInsets.only(
-                top: 24,
-                bottom: 24,
+                top: 12,
+                bottom: 12,
               ),
               child: Text(
                 _message,
                 style: TextStyle(
                   fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 8,
+                bottom: 8,
+              ),
+              child: Text(
+                _result,
+                style: TextStyle(
+                  fontSize: 16,
                   fontWeight: FontWeight.bold,
                 ),
               ),
@@ -95,30 +168,30 @@ class _HomePageState extends State<HomePage> {
                 GestureDetector(
                   onTap: () => _play("rock"),
                   child: Padding(
-                    padding: EdgeInsets.all(16),
+                    padding: EdgeInsets.all(8),
                     child: Image.asset(
-                      "images/lizard.png",
-                      height: 80,
+                      "images/rock.png",
+                      height: 70,
                     ),
                   ),
                 ),
                 GestureDetector(
                   onTap: () => _play("paper"),
                   child: Padding(
-                    padding: EdgeInsets.all(16),
+                    padding: EdgeInsets.all(8),
                     child: Image.asset(
-                      "images/lizard.png",
-                      height: 80,
+                      "images/paper.png",
+                      height: 70,
                     ),
                   ),
                 ),
                 GestureDetector(
-                  onTap: () => _play("scissors"),
+                  onTap: () => _play("scissor"),
                   child: Padding(
-                    padding: EdgeInsets.all(16),
+                    padding: EdgeInsets.all(8),
                     child: Image.asset(
-                      "images/lizard.png",
-                      height: 80,
+                      "images/scissor.png",
+                      height: 70,
                     ),
                   ),
                 ),
@@ -130,25 +203,57 @@ class _HomePageState extends State<HomePage> {
                 GestureDetector(
                   onTap: () => _play("lizard"),
                   child: Padding(
-                    padding: EdgeInsets.all(16),
+                    padding: EdgeInsets.all(8),
                     child: Image.asset(
                       "images/lizard.png",
-                      height: 80,
+                      height: 70,
                     ),
                   ),
                 ),
                 GestureDetector(
                   onTap: () => _play("spock"),
                   child: Padding(
-                    padding: EdgeInsets.all(16),
+                    padding: EdgeInsets.all(8),
                     child: Image.asset(
-                      "images/lizard.png",
-                      height: 80,
+                      "images/spock.png",
+                      height: 70,
                     ),
                   ),
                 ),
               ],
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 12,
+                bottom: 8,
+              ),
+              child: Text(
+                "Vitórias: " + _vitorias.toString(),
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                bottom: 8,
+              ),
+              child: Text(
+                "Derrotas: " + _derrotas.toString(),
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(),
+              child: Text(
+                "Empates: " + _empates.toString(),
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
           ],
         ),
       ),
